14325c57d275cd37f8b859165243028e821c2998
piyonosuke <piyonosuke21@gmail.com>
2021-01-20 07:11:41 +0000

    add current_user to tasksController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,22 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.all
   end
   
   def show
+    
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     
     if @task.save
       flash[:success] = 'Task は正常に作成されました'
@@ -40,20 +42,25 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.where(id: params[id])
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
